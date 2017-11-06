e6382ca77c33e493da0d183353925ca0fe40c122
amon <amon4254@gmail.com>
2017-11-06 16:15:13 +0700

    Create Fragment OK

---
+++ app/src/main/java/app/sirikhamhom/amon/rmutsvservice/MainFragment.java
@@ -0,0 +1,23 @@
+package app.sirikhamhom.amon.rmutsvservice;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Windows10 on 6/11/2560.
+ */
+
+public class MainFragment extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        return view;
+    }
+} // Main Class
